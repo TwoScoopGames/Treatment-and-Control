cfbfc66eb42bba571f0161842775b1bbcbc07810
Eric Lathrop <eric@ericlathrop.com>
2015-08-22 15:51:57 -0400

    Texturepacker refactoring

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ var mat4 = require("gl-matrix").mat4;
 var mvMatrix = mat4.create();
 var pMatrix = mat4.create();
 
-function drawScene(gl, shaderProgram, squareVertexPositionBuffer, texture, textureCoords) {
+function drawScene(gl, shaderProgram, squareVertexPositionBuffer, spriteName) {
 	gl.viewport(0, 0, canvas.width, canvas.height);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	mat4.perspective(pMatrix, 45 * Math.PI / 180.0, canvas.width / canvas.height, 0.1, 100.0);
@@ -45,12 +45,13 @@ function drawScene(gl, shaderProgram, squareVertexPositionBuffer, texture, textu
 	gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
 	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
-	if (texture.coords) {
-		var coords = texture.coords[textureCoords].buffer;
+	var sprite = sprites[spriteName];
+	if (sprite) {
+		var coords = sprite.coords
 		gl.bindBuffer(gl.ARRAY_BUFFER, coords);
 		gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, coords.itemSize, gl.FLOAT, false, 0, 0);
 		gl.activeTexture(gl.TEXTURE0);
-		gl.bindTexture(gl.TEXTURE_2D, texture);
+		gl.bindTexture(gl.TEXTURE_2D, sprite.texture);
 		gl.uniform1i(shaderProgram.samplerUniform, 0);
 	}
 
@@ -64,14 +65,38 @@ function setMatrixUniforms(gl) {
 }
 
 var textureJson = require("./images/test-spritesheet.json");
-function loadTexture(gl, image, json) {
+var sprites = {};
+function initTexture(gl) {
+	var image = new Image();
+	image.onload = function() {
+		var texture = imageToTexture(gl, image);
+		sprites = parseSpritesFromTexture(gl, texture, textureJson);
+	}
+	image.src = "images/test-spritesheet.png";
+}
+
+function imageToTexture(gl, image) {
+	var texture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	gl.bindTexture(gl.TEXTURE_2D, null);
+	texture.width = image.width;
+	texture.height = image.height;
+	return texture;
+}
+
+function parseSpritesFromTexture(gl, texture, json) {
 	return json.frames.reduce(function(sprites, frame) {
-		var left = frame.frame.x / image.width;
-		var top = 1.0 - (frame.frame.y / image.height);
-		var right = (frame.frame.x + frame.frame.w) / image.width;
-		var bottom = (image.height - frame.frame.y - frame.frame.h) / image.height;
+		var left = frame.frame.x / texture.width;
+		var top = 1.0 - (frame.frame.y / texture.height);
+		var right = (frame.frame.x + frame.frame.w) / texture.width;
+		var bottom = (texture.height - frame.frame.y - frame.frame.h) / texture.height;
 		sprites[frame.filename] = {
-			buffer: buildBuffer(gl, 2, [
+			texture: texture,
+			coords: buildBuffer(gl, 2, [
 				right, top,
 				left, top,
 				right, bottom,
@@ -82,27 +107,7 @@ function loadTexture(gl, image, json) {
 		};
 
 		return sprites;
-	}, {});
-}
-
-function initTexture(gl) {
-	var texture = gl.createTexture();
-	var image = new Image();
-	image.onload = function() {
-		handleLoadedTexture(gl, texture, image);
-		texture.coords = loadTexture(gl, image, textureJson);
-	}
-	image.src = "images/test-spritesheet.png";
-	return texture;
-}
-
-function handleLoadedTexture(gl, texture, image) {
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-	gl.bindTexture(gl.TEXTURE_2D, null);
+	}, sprites);
 }
 
 var canvas = document.getElementById("canvas");
@@ -117,11 +122,11 @@ var squareVertexPositionBuffer = buildBuffer(gl, 3, [
 	 1.0, -1.0,  0.0, // bottom right
 	-1.0, -1.0,  0.0 // bottom left
 ]);
-var texture = initTexture(gl);
+initTexture(gl);
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.enable(gl.DEPTH_TEST);
 function render() {
-	drawScene(gl, shaderProgram, squareVertexPositionBuffer, texture, "cartboy.png");
+	drawScene(gl, shaderProgram, squareVertexPositionBuffer, "cartboy.png");
 	window.requestAnimationFrame(render);
 }
 window.requestAnimationFrame(render);
