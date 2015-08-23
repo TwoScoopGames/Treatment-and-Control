95c1dd1c464cb4f47f2cf25441b01f7de3727901
Eric Lathrop <eric@ericlathrop.com>
2015-08-23 17:05:48 -0400

    Generate names

--- scripts/main-enter.js
+++ scripts/main-enter.js
@@ -1,7 +1,43 @@
 "use strict";
 
+function shuffle(o) {
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+}
+
+var starterNames = [
+  "Alex",
+  "Eric",
+  "Allen",
+  "Jeffrey",
+  "David",
+  "Rex",
+  "Clay",
+  "Sai",
+  "Chris",
+  "Barry",
+  "Loi",
+  "Cara",
+  "Tiffani",
+  "Stephen",
+  "Tyler",
+  "Brad",
+  "Benjamin",
+  "Jules",
+  "Aaron"
+];
+
 module.exports = function(data) {
   data.sounds.play("day1", true);
+  var names = starterNames.slice(0);
+  shuffle(names);
+  Object.keys(data.entities.entities).forEach(function(id) {
+    var entity = data.entities.entities[id];
+    if (entity.fadePercent === undefined) {
+      return;
+    }
+    entity.name = names.pop();
+  });
   
   var cart = data.entities.entities[3];
   
