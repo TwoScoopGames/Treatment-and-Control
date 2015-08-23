6de8fa58b0bfd7b0afb80981ebd2d09a741999c7
Eric Lathrop <eric@ericlathrop.com>
2015-08-23 10:49:47 -0400

    Better text box

--- systems/renderer/render-player.js
+++ systems/renderer/render-player.js
@@ -2,13 +2,16 @@
 
 module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 	ecs.addEach(function(entity, context) { // eslint-disable-line no-unused-vars
+		var x = 100;
+      	var y = 500;
+      
       	if (!entity.message) {
           return;
         }
-		context.drawImage(data.images.get("textbox"), 100, 100);
-      	context.fillStyle = "red";
-		context.font = "50px sans-serif";
+		context.drawImage(data.images.get("textbox"), x, y);
+      	context.fillStyle = "white";
+		context.font = "50px mono";
 		var msg = entity.message.text.substr(0, entity.message.len);
-		context.fillText(msg, 200, 200);
+		context.fillText(msg, x + 50, y + 100);
 	}, ["player", "message"]);
 };
