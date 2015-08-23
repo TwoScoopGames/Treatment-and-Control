9677943ba169d8c3b95836d700926e8143a4b2c0
Alex Bezuska <abezuska@gmail.com>
2015-08-23 13:39:06 -0400

    add bopundry and pipes

---
+++ systems/simulation/workerColor.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
+	ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
+      console.log(entity);
+	}, ["fadePercent", "fadepercent"]);
+};
