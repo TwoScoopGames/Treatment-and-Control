7741e51eee8376b395c04c40d6fdb9b81f4d0a2c
Eric Lathrop <eric@ericlathrop.com>
2015-08-23 15:54:58 -0400

    Pick up pill message.

--- scripts/main-enter.js
+++ scripts/main-enter.js
@@ -2,4 +2,10 @@
 
 module.exports = function(data) {
   data.sounds.play("day1", true);
+  
+  var cart = data.entities.entities[3];
+  
+  cart.deliveries = [
+    { name: "Rex", pill: "blue" }
+  ];
 };
--- systems/simulation/do-action.js
+++ systems/simulation/do-action.js
@@ -16,13 +16,26 @@ module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
             } else {
               for (var i = 0; i < entity.collisions.length; i++) {
                 var other = data.entities.entities[entity.collisions[i]];
-                if (!other.message) {
-                  continue;
+                if (other.message) {
+                  entity.message = { text: other.message.text, len: 0 };
+                  entity.timers.text.running = true;
+                  data.sounds.play("textpopup2");
+                  break;
                 }
-                entity.message = { text: other.message.text, len: 0 };
-                entity.timers.text.running = true;
-                data.sounds.play("textpopup2");
-                break;
+                if (other.deliveries) {
+                  var left = other.deliveries.filter(function(d) {
+                    return !d.done;
+                  });
+                  if (left.length === 0) {
+                    entity.message = { text: "No pills left.", len: 0 };
+                    entity.timers.text.running = true;
+                    data.sounds.play("textpopup2");                    
+                  } else {
+                    entity.message = { text: left[0].name + " gets the\n" + left[0].pill + " pill.", len: 0 };
+                    entity.timers.text.running = true;
+                    data.sounds.play("textpopup2");                                        
+                  }
+                }
               }
             }
           }
