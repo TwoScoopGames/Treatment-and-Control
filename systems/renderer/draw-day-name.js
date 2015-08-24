6f556dd9bda0659c362974a8fb3d69fd8fd04327
Eric Lathrop <eric@ericlathrop.com>
2015-08-24 16:10:49 -0400

    Use day names

--- systems/renderer/draw-day-name.js
+++ systems/renderer/draw-day-name.js
@@ -1,10 +1,11 @@
 "use strict";
 
+var days = [ "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY" ];
 module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 	ecs.add(function(entities, context) { // eslint-disable-line no-unused-vars
       context.font = "50px minecraftia";
       context.fillStyle = "#666";
       var day = data.arguments.day || 1;
-	  context.fillText("DAY " + day, 300, 300);
+	  context.fillText(days[(day - 1) % 5], 300, 300);
 	}, []);
 };
