d4ba8be216e53444647ecf40ca2df8559364d07e
Eric Lathrop <eric@ericlathrop.com>
2016-01-02 20:36:21 -0500

    Draw UI images

--- src/systems/renderer/render-player.js
+++ src/systems/renderer/render-player.js
@@ -19,38 +19,37 @@
 // 	context.fillText(line, x, y);
 // }
 
-module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
+module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 	ecs.addEach(function renderPlayer(entity, context) { // eslint-disable-line no-unused-vars
-		// var x = 0;
-		// var y = 436;
+		var x = 0;
+		var y = 436;
 
-		// var target = data.entities.get(entity, "target");
-		// var clipboard = data.entities.get(entity, "clipboard");
-		// var message = data.entities.get(entity, "message");
-		// if (target && clipboard) {
-		// 	var cart = 3;
-		// 	var deliveries = data.entities.get(cart, "deliveries");
+		var target = game.entities.get(entity, "target");
+		var clipboard = game.entities.get(entity, "clipboard");
+		var message = game.entities.get(entity, "message");
+		if (target && clipboard) {
+			var cart = 3;
+			var deliveries = game.entities.get(cart, "deliveries");
 
-		// 	context.drawImage(data.images.get("clipboard-big"), 400, 50);
+			game.renderer.drawSprite("clipboard-big.png", 400, 50);
 		// 	context.font = "24px minecraftia";
 		// 	context.fillStyle = "#666";
 
-		// 	for (var i = 0; i < deliveries.length; i++) {
-		// 		y = 200 + (i * 60);
-		// 		context.fillText(deliveries[i].name, 560, y + 45);
-		// 		context.drawImage(data.images.get("pill-" + deliveries[i].pill), 510, y);
-		// 		if (deliveries[i].done) {
-		// 			context.drawImage(data.images.get("strikeout"), 480, y + 18);
-		// 		}
-		// 	}
-		// }
-		// if (message) {
-		// 	context.drawImage(data.images.get("textbox-big"), x, y);
+			for (var i = 0; i < deliveries.length; i++) {
+				y = 200 + (i * 60);
+				// context.fillText(deliveries[i].name, 560, y + 45);
+				game.renderer.drawSprite("pill-" + deliveries[i].pill + ".png", 510, y);
+				if (deliveries[i].done) {
+					game.renderer.drawSprite("strikeout.png", 480, y + 18);
+				}
+			}
+		}
+		if (message) {
+			game.renderer.drawSprite("textbox-big.png", x, y);
 		// 	context.fillStyle = "white";
 		// 	context.font = "24px minecraftia";
 		// 	var msg = message.text.substr(0, message.len);
 		// 	wrapText(context, msg, x + 64, y + 64, 950, 30);
-		// }
-
+		}
 	}, "actionZone");
 };
